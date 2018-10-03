0e1f6d0389175ea06e7f2e5f06da54c624dacf6c
Anders Eriksson <anderse@unseen.is>
2018-10-03 15:10:08 +0200

    migrated tables to db, created models for media, genre, review

--- database/migrations/2018_10_03_125235_reviews.php
+++ database/migrations/2018_10_03_125235_reviews.php
@@ -4,21 +4,21 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class Reviews extends Migration
 {
     /**
      * Run the migrations.
      *
      * @return void
      */
+    
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('reviews', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->integer('level');
+            $table->string('title');
+            $table->string('author');
+            $table->text('text');
         });
     }
 
@@ -29,6 +29,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        //
     }
 }
