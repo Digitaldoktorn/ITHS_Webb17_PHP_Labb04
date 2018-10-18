e2fd2e21f33ce9b51a600655885faa369702c7bd
Mallarden <antonanderson.com@gmail.com>
2018-10-19 01:58:15 +0200

    new favicon and new routes if not logged in

--- resources/views/media/index.blade.php
+++ resources/views/media/index.blade.php
@@ -7,38 +7,32 @@
             <h1>Books</h1>
 
             @foreach ($media as $medium)
-              <div class="card">
+              <div class="card"  style="margin-top:20px;">
                   <div class="card-header"><a href="{{ route('media.show', $medium) }}">
                   {{ $medium->title }}
                   </a></div>
                   <div class="card-body">
                     {{ $medium->ISBN }}, {{ $medium->author }}, {{ $medium->genre }}
+                    <form action="{{ route('media.destroy',$medium) }}" method="POST" style="float:right;">
+
+                        <a class="btn btn-info" href="{{ route('media.show',$medium) }}">Show</a>
+
+                        <a class="btn btn-primary" href="{{ route('media.edit',$medium) }}">Edit</a>
+
+                        @csrf
+
+                        @method('DELETE')
+
+                        <button type="submit" class="btn btn-danger">Delete</button>
+
+                    </form>
+
                   </div>
               </div>
               @if (Auth::user() && Auth::user()->type == 'admin')
               <td>
 
-    <form action="{{ route('media.destroy',$medium) }}" method="POST">
-
-
-
-        <a class="btn btn-info" href="{{ route('media.show',$medium) }}">Show</a>
-
-
-
-        <a class="btn btn-primary" href="{{ route('media.edit',$medium) }}">Edit</a>
-
-
-
-        @csrf
-
-        @method('DELETE')
-
-
-
-        <button type="submit" class="btn btn-danger">Delete</button>
-
-    </form>
+ 
 
 </td>
 @endif
--- resources/views/reviews/index.blade.php
+++ resources/views/reviews/index.blade.php
@@ -6,11 +6,10 @@
         <div class="col-md-8">
             <h1>Reviews</h1>
             @foreach ($reviews as $review)
-              <div class="card">
-                  <div class="card-header">{{ $review->name }}</div>
-                  <div class="card-body">
-                    {{ $review->title }}, {{ $review->review }}
-
+              <div class="card" style="margin-top:20px;">
+                  <div class="card-header"><span style="text-decoration:underline;">{{ $review->title }} </span> by {{ $review->name }}</div>
+                  <div class="card-body"> 
+                      {{ $review->review }}
                   </div>
               </div>
             @endforeach
